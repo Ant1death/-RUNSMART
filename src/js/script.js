8ceee1b12c2d7f8df01519134a7db87abb643214
Ant1death <Ant1death@mail.ru>
2021-04-05 21:31:55 +0300

    5 commit

--- src/js/script.js
+++ src/js/script.js
@@ -8,9 +8,10 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 992,
                 settings: {
                 dots: true,
+                dotsClass: 'slick-dots',
                 arrows: false,
                 }
             }
@@ -22,21 +23,6 @@ $(document).ready(function(){
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
-
-    //   $('.catalog-item__link').each(function(i){
-    //       $(this).on('click', function(e) {
-    //           e.preventDefault();
-    //           $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //           $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    //       })
-    //   });
-    //   $('.catalog-item__back').each(function(i){
-    //     $(this).on('click', function(e) {
-    //         e.preventDefault();
-    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    //     })
-    // });
 
     function toggleslide(item) {
         $(item).each(function(i){
